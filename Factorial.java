5fd14e1483b445ff7cc3a963aad3e7db4a6300b6
sujatha-94 <sujathakarri701@gmail.com>
2024-07-31 14:47:27 +0530

    programs

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Factorial{
 // Write Your Code Here
+public static void main(String[] args)
+{
+    Scanner s=new Scanner(System.in);
+    int temp=1;
+    int a=s.nextInt();
+    for(int i=1;i<=a;i++)
+    {
+        temp=temp*i;	
+    }
+    System.out.println("factorial:"+temp);
 }
+}
+
--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class PerfectNumber{
 // Write Your code here
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter your Number");
+	int temp=0;
+	int a=s.nextInt();
+	for(int i=1;i<a;i++) {
+		if(a%i==0)
+        {
+			temp=temp+i;
+			System.out.println(i);
+		}	
+	}
+	if(temp==a) {
+		System.out.println("Perfect Number"+a);
+	}
+}
 }
