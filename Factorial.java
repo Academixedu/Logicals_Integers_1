fd89b4448b28f60f759d78f49840df0afc512cd4
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-07-31 15:44:32 +0530

    Assignment

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,13 @@
+
 public class Factorial{
 // Write Your Code Here
+public static void main(String[] args) {
+    //Scanner s=new Scanner(System.in);
+    int num=1;
+    int a=9;
+    for(int i=1;i<=a;i++) {
+    num=num*i;	
+    }
+    System.out.println("factorial:"+num);
+    }
 }
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Fibonacci{
 // Write your code here
+public void scan() {
+	int a,b,c,d;	
+	Scanner n=new Scanner(System.in);
+	System.out.println("Give Your first Number");
+	a=n.nextInt();
+	System.out.println("Give Second Number");
+	b=n.nextInt();
+	System.out.println("Give Your Count");
+	d=n.nextInt();
+	System.out.println("First Number"+a);
+	System.out.println("Second Number"+b);
+for(int e=0;e<d;e++) {
+c=a+b;
+System.out.print(c+",");
+a=b;
+b=c;
+}
+}	
+	public static void main(String[] args) {
+		Fibonacci f=new Fibonacci();
+         f.scan();		
+
+
+}
 }
