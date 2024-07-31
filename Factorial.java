22b8189ab18b03535ed864be80b9452faeae8e48
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-31 16:51:18 +0530

    commit

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Factorial{
 // Write Your Code Here
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a number");
+    int fn=sc.nextInt();
+    int temp=1;
+    for(int i=1;i<=fn;i++){
+        
+        temp *=i;       
+    } 
+    System.out.println(temp);
+    }
 }
+
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Fibonacci{
 // Write your code here
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a value");
+    int a=sc.nextInt();
+    System.out.println("enter b value");
+    int b=sc.nextInt();
+    int c;
+    System.out.println("enter a limit");
+    int limit=sc.nextInt();
+    for(int i=0;i<limit;i++){
+        c=a+b;//1 2 c=3 5
+              //a b
+              //  a b
+        System.out.println(c);
+        a=b;
+        b=c;
+    }
+}
 }
