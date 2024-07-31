aa623e43ae1f9521bfe4c1364bc5ed8311bc728d
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-07-31 19:54:12 +0530

    sarvesh

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
 public class Factorial{
 // Write Your Code Here
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the value:");
+int a=s.nextInt();
+int temp=1;
+for(int i=1;i<=a;i++){
+     temp=temp*i;
+}
+System.out.println(" The Factorial of "+a+" is:"+temp);
+}
 }
