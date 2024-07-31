ece9a7838da9f9501d11b516554393dc4acf3161
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-31 16:10:31 +0530

    kavitha

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class Factorial{
 // Write Your Code Here
+public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+int temp=1;
+int a=in.nextInt();
+for(int i=1;i<=a;i++) {
+temp=temp*i;	
 }
+System.out.println("factorial:"+temp);
+}
+}
+    
