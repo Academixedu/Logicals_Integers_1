f2b5df1ac1a0611fbc3116c5bef69be2528ae7e3
fayazshaik515 <fayazshaik231104@gmail.com>
2024-07-31 14:58:05 +0530

    from fayaz

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
 public class PerfectNumber{
-// Write Your code here
+public static void main(String[] args) {
+    
+     Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int a=sc.nextInt();
+        int temp=0;
+
+        for(int i=1;i<a;i++){
+           if(a%i==0){
+            temp =temp+a;
+            System.out.println(a);
+           
+        }
+    
+           if(temp==a){
+            System.out.println("perfect no "+a);
+           }
+           }
 }
+}
+    
+    
+    
+// Write Your code here
+
