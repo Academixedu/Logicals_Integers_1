16eea80e7688758afaf61a22f55a7578b4860a61
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-01 15:18:28 +0530

    from sai

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Fibonacci{
 // Write your code here
+public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.println("enter the value");
+    int a=in.nextInt();
+    System.out.println("enter the sequential value");
+    int b=in.nextInt();
+    System.out.println("enter the count");
+    int d=in.nextInt();
+    System.out.println("fibnacci series");
+    for(int e=0;e<=d;e++){
+        int c=a+b;
+        System.out.println(c+",");
+        a=b;
+        b=c;
+    }
+    
+}
 }
--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
 public class Palindrome{
 // Write Your code here
+
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("enter the value");
+        int a=in.nextInt();
+        int temp=0;
+        int pal=a;
+        while(a!=0){
+            int rem=a%10;
+            temp=temp*10+rem;
+            a=a/10;
+
+        }
+        if(temp==pal){
+            System.out.println("palindrome "+temp);
+        }
+        else{
+            System.out.println("reverse "+temp);
+              }
+
+    }
 }
+
+
+
+
