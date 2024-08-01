f8821bf309889a798c38913c5d6f1cbb78c8ff36
psvsai <pasupuletisvsm245@gmail.com>
2024-08-01 09:46:49 +0530

    Logicspsv

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class Factorial{
 // Write Your Code Here
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int temp = 1;
+    int a = sc.nextInt();
+    for (int i=1; i<=a; i++){
+        temp = temp*i;
+    } 
+    System.out.println("FACTORIAL of a is : " + temp);
+}
 }
