da65dd41872e5aaf53150764a66c3692e46076f2
Bharathmanne18 <bharathmanne12@gmail.com>
2024-08-02 18:15:00 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,29 @@
+ import java.util.Scanner;
 public class Palindrome{
+    public void palin(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter number");
+        int f=in.nextInt();
+        int temp=0;
+        int pal=f;
+        while(f!=0){
+            int rem = f%10;
+            temp=temp*10+rem;
+            f=f/10;
+        }
+        if(temp==pal){
+        System.out.println("palindrome is :"+temp);
+
+    }
+    else{
+        System.out.println("not palindrome");
+    }
+}
+    public static void main(String[] args){
+        Palindrome fa = new Palindrome();
+        fa.palin();
+
+
+    }
 // Write Your code here
 }
