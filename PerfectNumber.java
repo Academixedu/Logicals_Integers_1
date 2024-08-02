622dc92b8d32a776210fd04a1ef650d4a88f2ba9
12Rameshwar <chavhanrameshwar13@gmail.com>
2024-08-02 18:56:27 +0530

    check

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,3 +1,40 @@
-public class PerfectNumber{
-// Write Your code here
+
+
+import java.util.Scanner;
+
+public class PerfectNumber
+{
+
+    void perfect()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number that you have to check");
+        int a=sc.nextInt();
+        int temp=0;
+
+        for (int i=1;i<a;i++)
+        {
+            if (a%i==0)
+            {
+                temp=temp+i;
+                System.out.println("Factors :"+i);
+            }
+        }
+        if (temp==a)
+            {
+                System.out.println(a+" is perfect number");
+            }
+            else 
+            {
+                System.out.println(a+" is not  perfect number");
+            }
+            sc.close();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new PerfectNumber().perfect();
+    }
 }
