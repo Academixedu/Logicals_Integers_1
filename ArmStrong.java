2def3c1550caff9feb8d4580aaee03376358c895
ganeshteki <ganeshteki2001@gmail.com>
2024-08-02 10:17:59 +0530

    logi

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
 public class ArmStrong{
 // Write your code here
+public int countdigits(int a){
+    int count=0;
+    while(a>0){
+        a=a/10;
+        count++;
+    }
+    return count;
+}
+    public void Arm(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=sc.nextInt();
+        int temp=a;
+        int n=countdigits(a);
+        int sum=0;
+        while(a>0){
+            int b=0;
+            b=a%10;
+            int power=1;
+            for(int i=1;i<=n;i++)
+            {
+             power=power*b;
+            }
+             sum=sum+power;
+             a=a/10;
+        }
+        if(sum==temp){
+        System.out.println(temp+"is an armstrong number");
+    }
+    else{
+        System.out.println(temp+"not a armstrong");S
+    }
+}
+    public static void main(String args[]){
+        ArmStrong s=new ArmStrong();
+          s.Arm();
+    }
+
 }
--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
 public class Palindrome{
 // Write Your code here
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+     System.out.println("enter a number");
+     int a=sc.nextInt();
+     int temp=0;
+     int pal=a;
+     while(a!=0){
+        int rem=a%10;
+        temp=temp*10+rem;
+        a=a/10;
+     }
+     if(temp==pal){
+     System.out.println(pal+"is palindrome number");
+     }
+     else{
+        System.out.println(pal+"is not a palindrome");
+     }
+}
 }
