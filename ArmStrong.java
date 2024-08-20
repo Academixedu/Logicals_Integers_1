8e74f039275982b424b311690b99d01d99b90524
Your Name <your@email.com>
2024-08-20 20:12:16 +0530

    Hemanth

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,22 @@
-public class ArmStrong{
-// Write your code here
+import java.util.Scanner;
+
+public class ArmStrong {
+
+    public boolean isArmstrong(int n) {
+        int original = n, sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+        return sum == original;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        ArmStrong arm = new ArmStrong();
+        System.out.println(number + " is an Armstrong number: " + arm.isArmstrong(number));
+    }
 }
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Fibonacci{
-// Write your code here
+    public static void main(String[] args) {
+        System.out.println("Enter yout number");
+        Scanner sc=new Scanner(System.in);
+        int count=sc.nextInt();
+    int n1=0, n2=1,i, n3;
+    System.out.print(n1+" "+n2);
+    for(i=2;i<count;i++){
+        n3=n1+n2;
+        System.out.print(" "+n3);
+        n1=n2;
+        n2=n3;
+    }
+}
 }
