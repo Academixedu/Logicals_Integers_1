99b51b93711e5461cb12676f25b9ca952ce08583
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-20 20:01:27 +0530

    logics done

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Palindrome{
 // Write Your code here
+   public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the Number:");
+    int a = in.nextInt();
+    int temp=0;
+    int pal=a;
+
+    while(a!=0) {
+         int rem = a%10;
+         temp=temp*10+rem;
+         a=a/10;
+    }
+
+    if(pal==temp) {
+
+        System.out.println("Palindrome:" + temp);
+    }
+
+    else {
+        System.out.println("Not a Palindrome Number:"+temp);
+    }
+   }
+
 }
