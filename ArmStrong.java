099b9ef555a65b18bc3e48ba14006bcda59d43b0
gsbhavani <suryagogadi02@gmail.com>
2024-08-20 19:41:48 +0530

    completed

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,22 @@
+
+import java.util.Scanner;
 public class ArmStrong{
 // Write your code here
+public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum = 0;
+    int temp = n;
+    while(n > 0){
+        int rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+    if(sum == temp){
+        System.out.println("Armstrong");
+    }
+    else{
+        System.out.println("Not Armstrong");
+    }
+}
 }
--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,18 @@
+
+import java.util.Scanner;
 public class Fibonacci{
 // Write your code here
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            c = a+b;
+            a = b;
+            b = c;
+        }
+    }
 }
