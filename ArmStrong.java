da3ecdcafab737c3331779b15414e2e82a266407
KOushikguptha <koushikguptha2003@gmail.com>
2024-08-20 19:42:01 +0530

    Logicals Integers_1

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,36 @@
-public class ArmStrong{
-// Write your code here
+
+public class ArmStrong {
+
+ 
+    public static boolean isArmstrong(int number) {
+        int originalNumber = number;
+        int sum = 0;
+        int digits = 0;
+
+      
+        while (number != 0) {
+            number /= 10;
+            digits++;
+        }
+
+        number = originalNumber;
+
+        while (number != 0) {
+            int remainder = number % 10;
+            sum += Math.pow(remainder, digits);
+            number /= 10;
+        }
+
+        return sum == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        int number = 254; 
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
 }
