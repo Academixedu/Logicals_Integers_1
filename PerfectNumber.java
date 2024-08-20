fb2477ff05ee04fb8c15b19165c94941b469faf8
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-08-20 19:29:59 +0530

    int Logics are done

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class PerfectNumber{
 // Write Your code here
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter the number: ");
+    int temp=0;
+    int a=scan.nextInt();
+    for(int i=1;i<a;i++){
+        if(a%i==0){
+            temp=temp*i;
+            System.out.println(i);
+        }
+        if(temp==a){
+            System.out.println("The entered number is a Perfect Number: "+a);
+        }
+        else{
+            System.out.println("The entered number is not a Perfect Number: "+a);
+        }
+    }
+}
 }
