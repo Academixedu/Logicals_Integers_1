0260bc3a22cf039651cff4eaec4592069dc30b49
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-20 19:28:43 +0530

    logics

--- ArmStrong.java
+++ ArmStrong.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class ArmStrong{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int sum=0;
+        int temp=a;
+        while(a>0){
+            int rem=a%10;
+            sum=sum+(rem*rem*rem);
+            a=a/10;
+        }
+        if(temp==sum){
+            System.out.println("Armstrong");
+        }
+        else{
+            System.out.println("Not Armstrong");
+        }
+    }
 // Write your code here
 }
