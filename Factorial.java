3fa8f4f129dad21fa100bb4890232740913b0717
Swaroopa-123 <swaroopagentinapalli@gmail.com>
2024-08-20 19:34:52 +0530

    Assignement

--- Factorial.java
+++ Factorial.java
@@ -1,3 +1,45 @@
+import java.util.Scanner;
 public class Factorial{
-// Write Your Code Here
+// Write your code here
+
+
+
+    // Write Your Code Here
+    public int countDigits(int num) {
+
+        int count = 0;
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+    public void Arm() {
+        int b;
+        int sum = 0;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Give your Value");
+        int a = s.nextInt();
+        int temp = a;
+        int n = countDigits(a); 
+        while (a > 0) {
+            b = a % 10;
+            int power = 1;
+            for (int i = 1; i <=n; i++) {
+                power *= b;
+            }
+            sum += power;
+            a = a / 10;
+        }
+        if (temp == sum) {
+            System.out.println(temp + " is an Factorial");
+        } else {
+            System.out.println(temp + " is not an Factorial");
+        }
+    }
+
+    public static void main(String[] args) {
+        Factorial m = new Factorial();
+        m.Arm();
+    }
 }
--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class PerfectNumber{
 // Write Your code here
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter your Number");
+	int temp=0;
+	int a=in.nextInt();
+	for(int i=1;i<a;i++) {
+		if(a%i==0) {
+			temp=temp+i;
+			System.out.println(i);
+		}
+
+	}
+	if(temp==a) {
+		System.out.println("Perfect Number"+a);
+	}
+
+}
 }
